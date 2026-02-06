c955d85367e21fbef7b8abd2bb3bab3036082f79
omkar karpe <omkarkarpe91@gmail.com>
2026-02-06 22:37:16 +0530

    It devloper 1 is commited the code

--- js/login_signup.js
+++ js/login_signup.js
@@ -1,14 +1,23 @@
-function togglePassword() {
-  const passInput = document.getElementById("pass");
-  const pass2Input = document.getElementById("pass2");
-  const icon = document.getElementById("toggleIcon");
-  if (passInput.type === "password") {
-    passInput.type = "text";
-    pass2Input.type = "text";
+function togglePassword(inputId, iconId) {
+  const input = document.getElementById(inputId);
+  const icon = document.getElementById(iconId);
+
+  if (input.type === "password") {
+    input.type = "text";
     icon.classList.replace("fa-eye", "fa-eye-slash");
   } else {
-    passInput.type = "password";
-    pass2Input.type = "password";
+    input.type = "password";
     icon.classList.replace("fa-eye-slash", "fa-eye");
+  }
+}
+
+function chackPass() {
+  const passInput = document.getElementById("pass");
+  const pass2Input = document.getElementById("pass2");
+  const span=document.getElementById("chackpass");
+  if(passInput!==pass2Input)
+  {
+      span.innerText="Password Mismatch";
+      span.style.color="red";
   }
 }
